8f4e3679625bef72c55f6cbbf6387a03bed466de
CarolNgwe24 <145802881+CarolNgwe24@users.noreply.github.com>
2026-07-24 13:04:11 +0200

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function generatePoem(event){
     event.preventDefault();
-
-    new Typewriter("#Poem", {
-        strings: "On a day like tainted heaven, gods in suits come-bands of saviours carrying branded water cans and cameras.",
-        autoStart: true,
+    
+    document.querySelector("#Poem").innerHTML = ""; 
+    
+    let typewriter = new Typewriter("#Poem", {
         delay: 1,
         cursor: "",
- });
+    });
+
+    typewriter
+        .typeString("On a day like tainted heaven, gods in suits come-bands of saviours carrying branded water cans and cameras.")
+        .start();
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
-
