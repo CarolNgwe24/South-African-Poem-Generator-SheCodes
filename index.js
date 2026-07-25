021260ba6c63808dfa7549186b38e40753ac1be1
CarolNgwe24 <145802881+CarolNgwe24@users.noreply.github.com>
2026-07-25 16:36:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+let instructionsInput = document.querySelector("#user-instruction");
+let apiKey = "1e1cb74ecefd5a7o05tdff4f07af3b19";
+let poemElement = document.querySelector("#poem");
+
 function generatePoem(event) {
   event.preventDefault();
 
-  document.querySelector("#Poem").innerHTML = "";
-
-  let apiKey = "1e1cb74ecefd5a7o05tdff4f07af3b19";
-  let topic = document.querySelector(".instructions").value;
+  let topic = instructionsInput.value;
   let prompt = `Write a South African poem about ${topic}`;
   let context =
     "South African poetry and separate each line with a <br/> and only contain four lines of the poem Sign the poem with 'SheCodes AI'; inside a <strong/> elements Do not include the title";
@@ -13,11 +14,16 @@ function generatePoem(event) {
     prompt
   )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating your South African poem...</div>`;
+
   axios.get(apiURL).then(displayPoem);
 }
 
 function displayPoem(response) {
   let poemText = response.data.answer;
+
+  poemElement.innerHTML = `<div id="Poem"></div>`;
 
   let typewriter = new Typewriter("#Poem", {
     delay: 1,
